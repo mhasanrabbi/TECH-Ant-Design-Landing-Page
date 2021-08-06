896a01fa2ec6f92da7706f556a3b6818dbad657d
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-08-06 13:07:22 +0600

    About Component

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import AppAbout from '../home/About';
 import AppHero from '../home/Hero';
 
 function AppHome() {
   return (
     <div className="main">
       <AppHero/>
+      <AppAbout/>
     </div>
   )
 }
